d050514dd74648edfd9b64203feca9881d4e475c
marino de jesus gomez salazar <marino.gomez@udea.edu.co>
2025-11-28 15:51:44 -0500

    funcion tabla y mensaje

--- js/juego.js
+++ js/juego.js
@@ -1,9 +1,71 @@
 angular.module("JuegoTragamonedas", [])
-.controller("ControladorTragamonedas", function ($scope) {
+.controller("ControladorTragamonedas", function ($scope, $timeout) {
+
     $scope.rodillos = [];
+
+    // Contadores
+    $scope.ganasteCount = 0;
+    $scope.casiCount = 0;
+    $scope.perdisteCount = 0;
+
     $scope.girar = function () {
-        for (let i = 0; i < 3; i++) {
-            $scope.rodillos[i] = new Simbolo();
-        }
+
+        // Limpiar mensaje anterior
+        var resultadoDiv = document.getElementById("resultado");
+        resultadoDiv.className = "";
+        resultadoDiv.innerHTML = "";
+
+        // Generar los 3 símbolos
+        $scope.rodillos = [
+            new Simbolo(),
+            new Simbolo(),
+            new Simbolo()
+        ];
+
+        // Esperar a que termine la animación completa
+        $timeout(function () {
+            evaluar();
+        }, 1300);
     };
+
+    function evaluar() {
+
+        var a = $scope.rodillos[0].indice;
+        var b = $scope.rodillos[1].indice;
+        var c = $scope.rodillos[2].indice;
+
+        console.log("Evaluando: ", a, b, c); // Para debug
+
+        var mensaje = "";
+        var clase = "";
+
+        if (a === b && b === c) {
+            mensaje = "GANASTE";
+            clase = "ganar";
+            $scope.ganasteCount++;
+        }
+        else if (a === b || a === c || b === c) {
+            mensaje = "CASI";
+            clase = "casi";
+            $scope.casiCount++;
+        }
+        else {
+            mensaje = "PERDISTE";
+            clase = "perder";
+            $scope.perdisteCount++;
+        }
+
+        console.log("Mensaje: ", mensaje, "Clase: ", clase); // Para debug
+
+        // Mostrar mensaje con estilo
+        var resultadoDiv = document.getElementById("resultado");
+        resultadoDiv.innerHTML = mensaje;
+        resultadoDiv.className = clase + " mostrar";
+
+        // Actualizar tabla
+        document.getElementById("ganasteCount").innerText = $scope.ganasteCount;
+        document.getElementById("casiCount").innerText = $scope.casiCount;
+        document.getElementById("perdisteCount").innerText = $scope.perdisteCount;
+    }
+
 });
--- js/simbolo.js
+++ js/simbolo.js
@@ -3,6 +3,6 @@ function Simbolo() {
     this.indice = Math.floor(Math.random() * 5) + 1;
 
     this.getImagen = function () {
-        return "imagenes/simbolos/simbolo" + this.indice + ".jpg";
+        return "imagenes/simbolos/simbolo" + this.indice + ".png";
     };
 }
